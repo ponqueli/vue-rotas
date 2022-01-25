86b5ade724d64c68e1f44eae292f9f1511123f57
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-01-24 22:54:40 -0300

    aprendendo a trabalhar com rotas

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="jumbotron">
+      <h1 class="display-4">Vue Router</h1>
+      <p class="lead">Adicionando rotas a Single Page Applications no Vue.</p>
+    </div>
+
+    <div class="container">
+      <router-link to="/" class="btn btn-info mb-2 mr-2">Home</router-link>
+      <router-link :to="{ path: '/contatos' }" class="btn btn-info mb-2 mr-2">Contatos</router-link>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+ 
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+#app {
+  font-family: 'Ubuntu', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import Contatos from './views/contatos/Contatos.vue'
+import Home from './views/Home.vue'
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode:'history',
+  routes: [
+    { path:'/contatos', component: Contatos }, // meus-contatos/contatos
+    { path:'/', component: Home}               // meus-contatos
+  ]
+})
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
