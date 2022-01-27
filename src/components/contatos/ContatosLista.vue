aeda9d0ad4706c88cf40aab708a66edabc8ebc6d
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-01-27 16:41:04 -0300

    rotas, alias, rotas coringas, rotas coringas dentro de um contexto

--- src/components/contatos/ContatosLista.vue
+++ src/components/contatos/ContatosLista.vue
@@ -1,8 +1,20 @@
 <template>
     <div>
         <h3 class="font-weight-light">Contatos</h3>
-        <ul class="list-group" v-if="contatos.length > 0">
-            <ContatosListaItem class="list-group-item" v-for="contato in contatos" :key="contato.id"
+
+        <div class="form-group">
+            <input 
+                type="search"
+                class="form-control"
+                placeholder="Buscar contatos"
+                @keyup.enter="buscar"
+                :value="$route.query.busca">
+        </div>
+
+        <hr>
+
+        <ul class="list-group" v-if="contatosFiltrados.length > 0">
+            <ContatosListaItem class="list-group-item" v-for="contato in contatosFiltrados" :key="contato.id"
                 :contato="contato" />
         </ul>
         <p v-else>Nenhum contato cadastrado</p>
@@ -27,7 +39,21 @@ export default {
             ]
         }
     },
+    computed:{
+        contatosFiltrados(){
+            const busca = this.$route.query.busca
+            return !busca 
+                    ? this.contatos
+                    : this.contatos.filter(contato => { return contato.nome.toLowerCase().includes(busca.toLowerCase()) })
+        }
+    },
     methods: {
+        buscar(event){
+            this.$router.push({
+                path:'/contatos',
+                query: { busca: event.target.value }
+            })
+        },
         voltar() {
             this.$router.back() //mais adequado nesse contexto
         }
